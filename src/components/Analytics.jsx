d57913b6d83e2c9d1922f95e035f61b075dd4328
Ali Hamza Ranjha <hamzaranjha093@gmail.com>
2023-11-29 18:17:37 +0500

    changed section of data analytics to web hosting

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -7,8 +7,8 @@ const Analytics = () => {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
         <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
         <div className='flex flex-col justify-center'>
-          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+          <p className='text-[#00df9a] font-bold '>WEB HOSTING DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Your Website Centrally</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
             molestiae delectus culpa hic assumenda, voluptate reprehenderit
